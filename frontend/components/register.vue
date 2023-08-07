881847dc4fbe4ffc7b3e1a7c34869381b763667e
Fulosophy <michael.colandrea@outlook.com>
2023-08-07 19:05:36 -0400

    Changed icon to nuxt logo as placeholder

--- frontend/components/register.vue
+++ frontend/components/register.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div class="sm:mx-auto sm:w-full sm:max-w-md">
-              <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-              <h2 class="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register for your account</h2>
+              <img class="mx-auto h-10 w-auto" src="/favicon.ico" alt="Your Company" />
+              <h2 class="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register for your account!</h2>
             </div>
 
             <div class=" sm:mx-auto sm:w-full sm:max-w-[480px]">
