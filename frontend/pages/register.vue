49898dae6deb15b4b5df796a8fa8d933c6da1a4d
Fulosophy <michael.colandrea@outlook.com>
2023-08-07 19:01:57 -0400

    Added register page with simple registration

---
+++ frontend/pages/register.vue
@@ -0,0 +1,14 @@
+<template>
+  <register/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Register from '~/components/register.vue';
+
+
+export default Vue.extend({
+    name: 'IndexPage',
+    components: { Register }
+})
+</script>
